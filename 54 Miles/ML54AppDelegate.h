9b22c1b75144ded241cfbd5f3ebe08faa7af80a8
hannylicious <hannylicious@gmail.com>
2014-08-29 09:46:21 -0500

    Additional Commit for testing purposes

--- 54 Miles/ML54AppDelegate.h
+++ 54 Miles/ML54AppDelegate.h
@@ -15,6 +15,6 @@
 @property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 @property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
 @property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
+/*This is a test*/
 
 @end
